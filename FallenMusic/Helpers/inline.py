7c8f5e0da62bb65fe8510e5072f27ce90c9eab15
Mᴜᴊʜᴇ ᴋʏᴀ ᴍᴇ ᴛᴏ ʙᴀᴄᴄʜᴀ ʜᴜ <125765947+CuteBaccha@users.noreply.github.com>
2023-03-07 02:51:26 +0530

    Update inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -51,14 +51,14 @@ pm_buttons = [
     ],
     [InlineKeyboardButton(text="ʜᴇʟᴩ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="fallen_help")],
     [
-        InlineKeyboardButton(text="❄ ᴄʜᴀɴɴᴇʟ ❄", url=config.SUPPORT_CHANNEL),
-        InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=config.SUPPORT_CHAT),
+        InlineKeyboardButton(text="🥰 𝔹𝔸𝔹𝕐 𝕁𝕆𝕀ℕ 𝕂ℝ 𝕃𝕆 🥰", url=config.SUPPORT_CHANNEL),
+        InlineKeyboardButton(text="💞 𝕀𝕊𝕂𝕆 𝔹ℍ𝕀 𝕂ℝ 𝕃𝕆 ℕ𝔸 💞", url=config.SUPPORT_CHAT),
     ],
     [
         InlineKeyboardButton(
-            text="☁️ sᴏᴜʀᴄᴇ ☁️", url="https://github.com/TheAnonymous2005/FallenMusic"
+            text="🔷 𝕃𝔼 𝕃𝕆𝕆 🔷", url="https://github.com/CuteBaccha/sonababy"
         ),
-        InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=config.OWNER_ID),
+        InlineKeyboardButton(text="💥 𝕃𝔸𝔻𝕂𝕀𝕐𝔸 𝕂𝔸 ℂℝ𝕌𝕊ℍ 💥", user_id=config.OWNER_ID),
     ],
 ]
 
@@ -71,14 +71,14 @@ gp_buttons = [
         )
     ],
     [
-        InlineKeyboardButton(text="❄ ᴄʜᴀɴɴᴇʟ ❄", url=config.SUPPORT_CHANNEL),
-        InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=config.SUPPORT_CHAT),
+        InlineKeyboardButton(text="🥰 𝔹𝔸𝔹𝕐 𝕁𝕆𝕀ℕ 𝕂ℝ 𝕃𝕆 🥰", url=config.SUPPORT_CHANNEL),
+        InlineKeyboardButton(text="💞 𝕀𝕊𝕂𝕆 𝔹ℍ𝕀 𝕂ℝ 𝕃𝕆 ℕ𝔸 💞", url=config.SUPPORT_CHAT),
     ],
     [
         InlineKeyboardButton(
-            text="☁️ sᴏᴜʀᴄᴇ ☁️", url="https://github.com/TheAnonymous2005/FallenMusic"
+            text="🔷 𝕃𝔼 𝕃𝕆𝕆 🔷", url="https://github.com/CuteBaccha/sonababy"
         ),
-        InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=config.OWNER_ID),
+        InlineKeyboardButton(text="💥 𝕃𝔸𝔻𝕂𝕀𝕐𝔸 𝕂𝔸 ℂℝ𝕌𝕊ℍ 💥", user_id=config.OWNER_ID),
     ],
 ]
 
@@ -91,8 +91,8 @@ helpmenu = [
         )
     ],
     [
-        InlineKeyboardButton(text="sᴜᴅᴏ", callback_data="fallen_cb sudo"),
-        InlineKeyboardButton(text="ᴏᴡɴᴇʀ", callback_data="fallen_cb owner"),
+        InlineKeyboardButton(text="💥 𝕃𝔸𝔻𝕂𝕀𝕐𝔸 𝕂𝔸 ℂℝ𝕌𝕊ℍ 💥", callback_data="fallen_cb sudo"),
+        InlineKeyboardButton(text="💥 𝕃𝔸𝔻𝕂𝕀𝕐𝔸 𝕂𝔸 ℂℝ𝕌𝕊ℍ 💥", callback_data="fallen_cb owner"),
     ],
     [
         InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="fallen_home"),
@@ -102,7 +102,7 @@ helpmenu = [
 
 
 help_back = [
-    [InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=config.SUPPORT_CHAT)],
+    [InlineKeyboardButton(text="🥰 𝔹𝔸𝔹𝕐 𝕁𝕆𝕀ℕ 𝕂ℝ 𝕃𝕆 🥰", url=config.SUPPORT_CHAT)],
     [
         InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="fallen_help"),
         InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close"),
